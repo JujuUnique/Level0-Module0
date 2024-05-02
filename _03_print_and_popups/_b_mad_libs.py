7498a4f3fd4c799193cf8833ebe3126ea0b4f2f1
JujuUnique <jujusonofmariam@gmail.com>
2024-05-01 18:22:45 -0700

    wah wha wah wah wahhhhhhhhhhhhhhh

--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -8,17 +8,16 @@ if __name__ == '__main__':
     window.withdraw()
     # Put this sentence in a pop-up message box:
     # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-
-    # Get the player to enter an adjective
-
+    adjective=simpledialog.askstring(title='i do not like making titles',prompt='enter a adjective, and make it muddy!')
+    messagebox.showinfo(message="If you find yourself having to cross a piranha-infested river, here's how to do it...")
     # Get the player to enter a type of liquid
-
+    liquid=simpledialog.askstring(title="kill piranhas with tar",prompt='enter a liquid, and make it tar!!!')
     # Get the player to enter a body part
-
+    bodypart=simpledialog.askstring(title="nothing lol",prompt='put in a body part, and make it hands!!!')
     # Get the player to enter a verb
-
+    verb=simpledialog.askstring(title="still nothing lol",prompt='enter a verb, and make it grab tar!!!')
     # Get the player to enter a place
-
+    place=simpledialog.askstring(title="no mind to break no will to think and no voice to cry suffering",prompt='put in a place, and make it lake!!!')
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
@@ -27,12 +26,13 @@ if __name__ == '__main__':
     #       other parts of the story.
 
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
+        "Piranhas are more "+adjective+" during the day, so cross the river at\n"
+        "night. Piranhas are attracted to fresh "+liquid+" and will most\n"
+        "likely take a bite out of your "+bodypart+" if you "+verb+". Whatever\n"
         "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        "back to the "+place+". Good luck!"
     )
+    messagebox.showinfo(message=story)
 
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
